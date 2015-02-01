5c7dc3e7dae5acf5b1700ab277b7dbc2e1119193
Max St-Onge <maxstonge@gmail.com>
2015-01-31 22:53:13 -0500

    Ajout de handle et residence

--- rez.rb
+++ rez.rb
@@ -5,23 +5,23 @@ require 'nokogiri'
 require 'rubygems'
 require 'rufus/scheduler'
 require 'twitter'
-require 'sinatra'
+#require 'sinatra'
 
-if Sinatra::Base.development?
-  require 'pry'
-end
+#if Sinatra::Base.development?
+#  require 'pry'
+#end
 
 class RezUsage
 
-	def initialize
-    url = "http://reznetusage.azurewebsites.net/?phase=2&appart=627"
+	def initialize(appartement)
+    url = "http://reznetusage.azurewebsites.net/?phase=#{appartement[:phase]}&appart=#{appartement[:appartement]}"
     @page = Nokogiri::HTML(open(url))
   end
 
   def parse_usage
 		
     gb_restant = @page.css('span#ContentPlaceHolder1_lblRestant').text.delete('GB').to_i
-    borne_atteinte = (0..20).step(5).find { |b| b >= gb_restant }
+    borne_atteinte = (0..100).step(5).find { |b| b >= gb_restant }
     if borne_atteinte
       "Vous avez atteint la borne de #{borne_atteinte} GB, il ne vous reste que #{gb_restant} GB"
     end
@@ -36,21 +36,23 @@ class TweetBot
 
   def configure_twitter
     @client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-    config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+    config.consumer_key        = '****'
+    config.consumer_secret     = '****'
+    config.access_token        = '****'
+    config.access_token_secret = '***'
   end
 
-  def send_tweet(msg)
-    if Sinatra::Base.production?
-      @client.update(msg)
-    else
-      puts "[#{Time.now()}] #{msg}"
-    end
+  def send_tweet(msg, handle)
+   # if Sinatra::Base.production?
+      @client.update("[#{Date.today.strftime("%Y %m")}] #{msg} @#{handle}")
+    #else
+     # puts "[#{Time.now()}] #{msg}"
+    #end
   end
 
 end
-
-message =RezUsage.new().parse_usage
-TweetBot.new.send_tweet(message) if message end
+end
+{goku417: {phase: 2, chambre: 626}, maxstonge: {phase: 2, chambre: 627}}.each do |handle, appartement|
+  message =RezUsage.new(appartement).parse_usage
+  TweetBot.new.send_tweet(message, handle) if message 
+end
