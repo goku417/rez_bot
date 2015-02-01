cf56847d0882cba2940798e2d86be386ebc5c011
Max St-Onge <maxstonge@gmail.com>
2015-01-31 23:59:21 -0500

    Clean code

--- rez.rb
+++ rez.rb
@@ -24,10 +24,9 @@ class RezUsage
     gb_restant = @page.css('span#ContentPlaceHolder1_lblRestant').text.delete('GB').to_i
     borne_atteinte = (0..100).step(5).find { |b| b >= gb_restant }
     if borne_atteinte
-      "Vous avez atteint la borne de #{borne_atteinte} GB, il ne vous reste que #{gb_restant} GB"
+      "Vous avez atteint la borne de #{borne_atteinte} GB, il ne vous reste que #{gb_restant} GB" end
     end
-	end
-end
+  end
 
 class TweetBot
   def initialize
@@ -37,27 +36,25 @@ class TweetBot
 
   def configure_twitter
     @client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = '**'
-    config.consumer_secret     = '***'
-    config.access_token        = '***'
-    config.access_token_secret = '****'
+      config.consumer_key        = '**'
+      config.consumer_secret     = '***'
+      config.access_token        = '***'
+      config.access_token_secret = '****'
+    end
   end
 
   def send_tweet(msg, handle)
-   # if Sinatra::Base.production?
+    if Sinatra::Base.production?
       @client.update("[#{Date.today.strftime("%Y %m")}] #{msg} @#{handle}")
-    #else
-     # puts "[#{Time.now()}] #{msg}"
-    #end
+    else
+      puts "[#{Date.today.strftime("%Y %m")}] #{msg} @#{handle}"
+    end
   end
-
-end
 end
 
 get '/' do
   {goku417: {phase: 2, chambre: 626}, maxstonge: {phase: 2, chambre: 627}}.each do |handle, appartement|
-  message =RezUsage.new(appartement).parse_usage
-  TweetBot.new.send_tweet(message, handle) if message 
-end
-
+    message =RezUsage.new(appartement).parse_usage
+    TweetBot.new.send_tweet(message, handle) if message
+  end
 end
